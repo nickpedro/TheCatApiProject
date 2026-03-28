684851dd2f6ba175079f200c804c676593ddb7dc
nickpedro <43183915+nickpedro@users.noreply.github.com>
2026-03-28 12:21:05 -0300

    integração com SQL Server e persistência de dados

--- TheCatApiProject/Controller/CatController.cs
+++ TheCatApiProject/Controller/CatController.cs
@@ -1,16 +1,22 @@
 ﻿using System.Threading.Tasks;
 using TheCatApiProject.Models;
 using TheCatApiProject.Service;
+using TheCatApiProject.Data;
 
 namespace TheCatApiProject.Controllers
 {
     public class CatController
     {
         private readonly CatService _service = new CatService();
+        private readonly CatRepository _repository = new CatRepository();
 
         public async Task<Cat> BuscarGato()
         {
-            return await _service.GetRandomCat();
+            var cat = await _service.GetRandomCat();
+
+            _repository.Salvar(cat); 
+
+            return cat;
         }
     }
 }
---
+++ TheCatApiProject/Data/CatRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using TheCatApiProject.Models;
+
+namespace TheCatApiProject.Data
+{
+    public class CatRepository
+    {
+        private readonly string _connectionString;
+
+        public CatRepository()
+        {
+            _connectionString = ConfigurationManager
+                .ConnectionStrings["SqlConnection"]
+                .ConnectionString;
+        }
+
+        public void Salvar(Cat cat)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                string query = @"INSERT INTO Cats (ApiId, Url, DataBusca)
+                                 VALUES (@ApiId, @Url, @DataBusca)";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@ApiId", cat.id);
+                cmd.Parameters.AddWithValue("@Url", cat.url);
+                cmd.Parameters.AddWithValue("@DataBusca", DateTime.Now);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
